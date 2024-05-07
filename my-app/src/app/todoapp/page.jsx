dec984fcbd3a90bfdc8ef76063bb270ccedb6791
abhinand-psq <103823872+abhinand-psq@users.noreply.github.com>
2024-05-07 14:13:27 +0530

    3rd commit

--- my-app/src/app/todoapp/page.jsx
+++ my-app/src/app/todoapp/page.jsx
@@ -1,12 +1,24 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import style from './page.module.css'
 function Page() {
 const [add, setadd] = useState('')
-const [list,setlist]=useState(['sdsfd'])
+const [list,setlist]=useState([])
+const [sample,setsample]=useState({name:"abhinand"})
+const [myObject, setMyObject] = useState({ name: "John", age: 30 });
+useEffect(() => {
+    setsample((val)=>{
+        return {...val,name:"adarsh",age:45}
+       })
+}, [])
 
 function running(){
 setlist(Val=>([...Val,add]))
+
+}
+
+async function sampleS(){
+   console.log(sample);
 }
 
     return (
@@ -14,7 +26,7 @@ setlist(Val=>([...Val,add]))
         <input type="text" name="" id="" onChange={(e)=>{setadd(e.target.value)}}/>
         <button onClick={()=>{running()}}>add</button>
         {
-            list&&list.map((res,ind)=>{
+            list.length != 0&&list.map((res,ind)=>{
                 return(
                     <div className={style.inp} key={ind}>
                         <p key={ind}>{res}</p>
@@ -22,8 +34,12 @@ setlist(Val=>([...Val,add]))
                     </div>
                 )
             })
+              
         }
+        
+<button onClick={()=>{sampleS()}}>click</button>
     </div>
+
   )
 }
 
